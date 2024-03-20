a008ba3410b62521c7476da7d2c45e7c8f1f1cc8
Antony Hu <ah20226@my.westlake.school.nz>
2024-03-20 23:36:03 +1300

    Added code for a black game window

--- code/Assessment/Game.py
+++ code/Assessment/Game.py
@@ -13,11 +13,34 @@ SCREEN_TITLE = "Advanced Combat"
 
 class MyGame(arcade.Window):
 
-    """ Set's up the game window, with the dimensions above height, width and title by calling the class 'MyGame' """
+    """ Set's up the game window, by calling the class 'MyGame' """
     def __init__(self):
 
         
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        if self.tile_map.background_color:
-            arcade.set_background_color(self.tile_map.background_color)
+        arcade.set_background_color(arcade.csscolor.BLACK)
+
+
+    def setup(self):
+        """Sets up the game, loads and creates game resources and objects."""
+
+        pass       
+
+
+    def on_draw(self):
+        """Renders the screen by drawing the game elements. Clears the screen with 'self.clear()' """
+
+        self.clear()
+
+
+def main():
+    """Main function for the program"""
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+"""The program checks to see if the program is running from the main file, and if it is then it starts the program. """
+
+if __name__ == "__main__":
+    main()
