1da48c96e21b31723eaded3e0b7d332179a20991
Antony Hu <ah20226@my.westlake.school.nz>
2024-03-20 17:47:20 +1300

    added more code

--- code/Assessment/Game.py
+++ code/Assessment/Game.py
@@ -1,10 +1,23 @@
-"""Main file"""
+""" Main file """
 
 import arcade
 
-"""Game window"""
+""" Game window """
 
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Advanced Combat"
 
+
+""" The class 'Mygame' is the main application """
+
+class MyGame(arcade.Window):
+
+    """ Set's up the game window, with the dimensions above height, width and title by calling the class 'MyGame' """
+    def __init__(self):
+
+        
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
