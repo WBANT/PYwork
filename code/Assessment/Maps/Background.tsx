48298a0925797f8a77a442d7cce75df948d6421b
Antony Hu <ah20226@my.westlake.school.nz>
2024-03-20 16:31:43 +1300

    added to map, and made python file

--- code/Assessment/Maps/Background.tsx
+++ code/Assessment/Maps/Background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Background" tilewidth="16" tileheight="16" tilecount="391" columns="23">
- <image source="D:/Assessments/Coding/Legacy Fantasy - Castle Prison/Legacy Fantasy - Castle Prison/Assets/Background.png" width="368" height="272"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Background" tilewidth="16" tileheight="16" tilecount="391" columns="23">
+ <image source="G:/Assessments/Coding/Legacy Fantasy - Castle Prison/Legacy Fantasy - Castle Prison/Assets/Background.png" width="368" height="272"/>
 </tileset>
--- code/Assessment/Maps/Tiles.tsx
+++ code/Assessment/Maps/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
- <image source="D:/Assessments/Coding/Legacy Fantasy - Castle Prison/Legacy Fantasy - Castle Prison/Assets/Tiles.png" width="400" height="400"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="G:/Assessments/Coding/Legacy Fantasy - Castle Prison/Legacy Fantasy - Castle Prison/Assets/Tiles.png" width="400" height="400"/>
 </tileset>
